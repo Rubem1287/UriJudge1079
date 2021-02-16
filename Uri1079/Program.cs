64a9a57fd5f7d8b26e1e10e26af6ba1f550bf27b
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:44:33 -0300

    Add files via upload

---
+++ Uri1079/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace Uri1079
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double m;
+            m = 0;
+          
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vetor = Console.ReadLine().Split(' ');
+                double a = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+                double b = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+                double c = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+                m = (double)((a * 2.0) + (b * 3.0) + (c * 5.0)) / (2.0 + 3.0 + 5.0);
+
+
+                Console.WriteLine(m.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
